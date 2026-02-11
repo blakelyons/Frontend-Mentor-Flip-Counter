5361bfe020ca850dc4b1e4139c11669e31c55f33
Blake Lyons <blake@blakelyons.com>
2026-02-11 11:06:29 -0500

    Various updates

--- src/components/FlipNumber.vue
+++ src/components/FlipNumber.vue
@@ -16,6 +16,32 @@ onMounted(() => {
     value: props.value
   })
 
+  // Remove any branding text that might be added by the library
+  const removeBranding = () => {
+    if (tick.value) {
+      const walker = document.createTreeWalker(
+        tick.value,
+        NodeFilter.SHOW_TEXT,
+        null
+      )
+      let node
+      while ((node = walker.nextNode())) {
+        if (node.textContent && /powered by|pqina/i.test(node.textContent)) {
+          node.textContent = ''
+        }
+      }
+      // Also remove any elements with branding-related classes or text
+      const brandingElements = tick.value.querySelectorAll(
+        '[class*="powered"], [class*="branding"], [class*="pqina"]'
+      )
+      brandingElements.forEach((el) => el.remove())
+    }
+  }
+
+  // Remove branding immediately and after a short delay (in case it's added dynamically)
+  removeBranding()
+  setTimeout(removeBranding, 100)
+
   onBeforeUnmount(() => {
     Tick.DOM.destroy(tick.value)
   })
@@ -24,6 +50,8 @@ onMounted(() => {
     () => props.value,
     (newValue) => {
       _tick.value = newValue
+      // Remove branding again after value update
+      setTimeout(removeBranding, 50)
     }
   )
 })
